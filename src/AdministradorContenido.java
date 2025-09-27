145d6e010f689158c5c1b2af76c5c048f0902a83
Blaster 9078 <blaster9078@hotmail.com>
2025-09-26 23:12:06 -0500

    Initial commit

---
+++ src/AdministradorContenido.java
@@ -0,0 +1,3 @@
+public class AdministradorContenido extends Usuario{
+    private
+}
---
+++ src/Usuario.java
@@ -0,0 +1,8 @@
+public abstract class Usuario {
+    private String id;
+    private String nombre;
+    private String passwordHash;
+    private String rol;
+    private String fechaRegistro;
+    private String estadoCuenta;
+}
